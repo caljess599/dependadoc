0226e8f17353fbd21144a6dacca76211ccc539dd
Jessica Otey <jessica.otey@iorahealth.com>
2022-05-26 11:23:26 -0400

    don't nest mirrored folder

--- dependadoc.sh
+++ dependadoc.sh
@@ -31,7 +31,7 @@ git config user.name $GITHUB_ACTOR
 git config user.email via-github-actions@github.com
 
 # copy files
-cp -rf $GITHUB_WORKSPACE/main/$MIRRORED_FOLDER  $DOCS_REPOSITORY_PATH/mirror-${MIRRORED_REPO}
+cp -rf $GITHUB_WORKSPACE/main/$MIRRORED_FOLDER/  $DOCS_REPOSITORY_PATH/mirror-${MIRRORED_REPO}
 
 # add changes
 git add .
